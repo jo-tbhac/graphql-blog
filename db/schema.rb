683b905d801cea5ddfca0f40c3143dc7ce117c0e
junpei ogawa <cornerstone.am3@gmail.com>
2020-11-15 20:37:53 +0900

    create model comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_113243) do
+ActiveRecord::Schema.define(version: 2020_11_15_113610) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "content_id", null: false
+    t.text "text", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_comments_on_content_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -28,5 +38,7 @@ ActiveRecord::Schema.define(version: 2020_11_15_113243) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "contents"
+  add_foreign_key "comments", "users"
   add_foreign_key "contents", "users"
 end
