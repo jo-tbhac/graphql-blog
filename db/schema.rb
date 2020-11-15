281f5d61afa0a55eee2e8d0adcf0e5412473ffe7
junpei ogawa <cornerstone.am3@gmail.com>
2020-11-15 20:35:57 +0900

    create model content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_112950) do
+ActiveRecord::Schema.define(version: 2020_11_15_113243) do
+
+  create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_contents_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nickname", null: false
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_11_15_112950) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "contents", "users"
 end
